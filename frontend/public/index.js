68d57815b77f40cd56cf2b23bb4e73e3b72c7b5f
eungyeong0927 <bb060@naver.com>
2024-07-31 15:37:18 +0900

    시험- 프론트_3

---
+++ frontend/public/index.js
@@ -0,0 +1,36 @@
+function addTodo(elem) {
+    const inputBoxElem = document.querySelector("#input-box");
+    const inputBoxValue = inputBoxElem.value;
+    if (inputBoxValue.trim()) {
+        document.querySelector(".to-do-list").innerHTML += `
+<li>
+<input type="checkbox" class="checkbox" onclick="onClickCheckbox(this)" />
+<span>${inputBoxValue}</span>
+<input type="button" id="delete-button" value="x" onclick="deleteTodo(this)" />
+</li>
+`;
+        inputBoxElem.value = "";
+    }
+}
+
+function enterKey() {
+    if (window.event.keyCode == 13) {
+        addTodo();
+    }
+}
+
+function deleteTodo(deleteElem) {
+    deleteElem.parentElement.remove();
+}
+
+function onClickCheckbox(checkboxElem) {
+    const liElem = checkboxElem.parentElement;
+    const spanElem = liElem.querySelector("span");
+    if (checkboxElem.checked) {
+        spanElem.innerHTML = `<s>${spanElem.innerText}</s>`;
+        spanElem.style.color = "gray";
+    } else {
+        spanElem.innerHTML = `${spanElem.innerText}`;
+        spanElem.style.color = "black";
+    }
+}
